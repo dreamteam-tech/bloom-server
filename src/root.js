05b917a0bf9dc7ef2b763785ae0b14ec07843c3d
Falaleev Maxim <maxfalaleev1@gmail.com>
2018-12-08 16:29:32 +0300

    enable playground in production

--- src/root.js
+++ src/root.js
@@ -3,9 +3,12 @@ const resolvers = require('./resolvers');
 const typeDefs = require('./typeDefs');
 
 module.exports = {
-  debug: false,
+  debug: process.env.NODE_ENV === 'production',
   typeDefs,
   resolvers,
   schemaDirectives,
-  context: ({ req }) => ({ req })
+  context: ({ req }) => ({ req }),
+  // https://www.apollographql.com/docs/apollo-server/features/graphql-playground.html
+  introspection: true,
+  playground: true,
 };
